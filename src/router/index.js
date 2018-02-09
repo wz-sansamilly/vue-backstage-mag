cf8b846100cd5589e3db249aa5ed6d1ff99ce7e9
wxj <sansamilly@sina.com>
2018-02-09 16:13:31 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,6 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Profile',
-      component: Profile
-    },
-    {
       path: '/profile',
       name: 'Profile',
       component: Profile
